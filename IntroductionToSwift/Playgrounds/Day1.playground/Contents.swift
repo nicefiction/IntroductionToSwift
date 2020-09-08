30e32993bbaeac2bd13ead66109d69ceb460d41c
Olivier Van hamme <nicefiction@gmail.com>
2020-09-08 19:03:40 +0200

    Completes Day 1 .

---
+++ IntroductionToSwift/Playgrounds/Day1.playground/Contents.swift
@@ -0,0 +1,75 @@
+import UIKit
+
+var str = "Hello, playground"
+
+/* Strings :
+ */
+
+str = "Good Bye 👋"
+
+
+
+/* Integers :
+ */
+
+var age = 38
+
+var population = 8_000_000
+
+
+
+/* Multiline strings
+ */
+
+var string1 = """
+Swift is very particular about
+how you write those quote marks :
+the opening and closing triple must be on their own line ,
+but opening and closing line breaks
+won’t be included in your final string .
+"""
+
+
+var string2 = """
+If you only want multi-line strings to format your code neatly , \
+and you don’t want those line breaks to actually be in your string , \
+end each line with a \\ .
+"""
+
+
+var burns = """
+The best laid schemes
+o' mice an' men
+gang aft agley
+"""
+
+
+
+/* Doubles and Booleans :
+ */
+
+var pi = 3.141
+var awesome = true
+
+
+
+/* String Interpolation :
+ */
+
+var score = 85
+var string3 = "Your score is \(score) ."
+var result = "Test Result : \(string3) ."
+
+
+
+/* Constants :
+ */
+
+let taylor = "Swift"
+
+
+
+/* Type annotations :
+ */
+
+let artist: String = "The Avalanches"
